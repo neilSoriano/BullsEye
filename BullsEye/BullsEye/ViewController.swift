ed53fae18b59a9d5c4d05b6155fb6718791ce3ec
Neil Soriano <33480769+neilSoriano@users.noreply.github.com>
2020-02-05 13:00:30 -0800

    Add files via upload

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Neil Soriano on 6/10/19.
+//  Copyright © 2019 Neil Soriano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var targetLabel : UILabel!
+    @IBOutlet weak var scoreLabel : UILabel!
+    @IBOutlet weak var roundLabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(){
+        
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+       
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        
+    }
+    
+    @IBAction func sliderMoved(_ slider : UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+        let roundedValue = slider.value.rounded()
+        print("The rounded value of the slider is now: \(roundedValue)")
+        currentValue = Int(slider.value)
+    }
+
+    func startNewRound(){
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+        
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+}
+       
+
+
